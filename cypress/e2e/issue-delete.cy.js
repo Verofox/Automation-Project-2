17a0a00a660cd81a730cd93ccc7434bb7029d80b
Verofox <rebanever@gmail.com>
2023-07-08 23:21:22 +0300

    Delete issue

---
+++ cypress/e2e/issue-delete.cy.js
@@ -0,0 +1,65 @@
+
+describe('Issue delete', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
+      cy.visit(url + '/board');
+      // Access first available issue
+      cy.get('[data-testid="board-list:backlog"]')
+        .children()
+        .first()
+        .click();
+      cy.get('[data-testid="modal:issue-details"]')
+        .should('be.visible');
+    });
+  });
+
+  let issueTitle = 'This is an issue of type: Task.'
+  const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
+
+  it('Deleting first issue from backlog list and verifying successful removal ', () => {
+    getIssueDetailsModal()
+      .find('[data-testid="icon:trash"]')
+      .click();
+    cy.get('[data-testid="modal:confirm"]')
+      .should('be.visible');
+    cy.get('[data-testid="modal:confirm"]')
+      .contains('Delete issue')
+      .click()
+      .should('not.exist');
+
+    cy.reload();
+
+    cy.get('[data-testid="board-list:backlog')
+      .should('be.visible')
+      .and('have.length', '1').within(() => {
+        // Assert that the first issue from the list has been deleted
+        cy.contains(issueTitle).should('not.exist');
+      });
+  });
+
+  it('Initiating the process of deleting the issue, but canceling the action.', () => {
+    getIssueDetailsModal()
+      .find('[data-testid="icon:trash"]')
+      .click();
+    cy.get('[data-testid="modal:confirm"]')
+      .should('be.visible');
+    cy.get('[data-testid="modal:confirm"]')
+      .contains('Cancel')
+      .click()
+      .should('not.exist');
+    cy.get('[data-testid="icon:close"]')
+      .first()
+      .click();
+
+    cy.reload();
+
+    cy.get('[data-testid="board-list:backlog')
+      .should('be.visible')
+      .and('have.length', '1').within(() => {
+        // Assert that the first issue from the list is visible
+        cy.contains(issueTitle).should('be.visible');
+      });
+  });
+});
+
